44ad25e6dd00e7e582169cfa8e38629562b3618c
takahashi kazuki <k.takahashi.sys22@morijyobi.ac.jp>
2022-11-08 12:35:13 +0900

    Main.javaに１回の実行で２人分計算できるように修正

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 
+		for (int i = 0; i < 2; i++){
 		System.out.print("身長:");
 		double height = sc.nextDouble();
  		System.out.print("体重:");
@@ -12,6 +13,7 @@ public class Main{
 		double bmi = (double)weight / (height * height);
 
 		System.out.println("BMI:" + String.format("%.2f", bmi));
+		}
 	}
 }
 
